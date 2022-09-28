fa1b2595b5d6362de795fd1343a7ca532f0e6101
Keshawn Bryant <91396292+BestBother@users.noreply.github.com>
2022-09-28 11:46:19 -0400

    Update and rename action_page.php to action_login_page.php

---
+++ action_login_page.php
@@ -0,0 +1,8 @@
+<?php
+
+		$email = $_GET['email'];
+		$psw = $_GET['psw'];
+		
+		echo "Your email address is: ".$email."<br>"; 
+		echo "Your password is: ".$psw."<br>";		
+?>
--- action_page.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-		echo "Your email address is: " , $_GET['psw']; 
-		echo "Your password is", $_GET['psw'];		
-?>
