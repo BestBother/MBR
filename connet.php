bbf6c2c8d6ec02412f498705fe8d3abb00261c23
Keshawn Bryant <91396292+BestBother@users.noreply.github.com>
2022-10-03 11:18:49 -0400

    Create connet.php
    poot

---
+++ connet.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "<h3>Connected successfully<h3>";
+?>
