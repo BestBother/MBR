69e0925dd01f04736588476403aba4c7ecea9aa7
Keshawn Bryant <91396292+BestBother@users.noreply.github.com>
2022-10-04 16:58:28 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,30 +2,34 @@
 $servername = "localhost";
 $username = "Bother";
 $password = "1234";
-$dbname = "myDBPDO";
-
-echo "<h1> Insert vaules below <h1>";
+$dbname = "mbr_test_database";
 
 $fname = $_POST['fname'];
 $email = $_POST['email'];
+$phonenum = $_POST['phonenum'];
+$psw = $_POST['psw'];
+$oPet = $_POST['oPet'];
 
-try {
-  $conn = new mysqli($servername, $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(mysqli::ATTR_ERRMODE, mysqli::ERRMODE_EXCEPTION);
-  
-  $sqli = "INSERT INTO owner (ownID, oName, email, psw, petID, oPet,phonenum)
-  VALUES ('12345678', 'Doe', 'john@example.com','1223','12345678','dog','5404467089')";
-  
-  // use exec() because no results are returned
- $conn->exec($sql);
-  echo "New record created successfully";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+$ownID = rand(10000000, 99999999);
+$petID = rand(10000000, 99999999);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-$conn = null;
+$sql = "INSERT INTO owner (ownID, oName, email, psw, oPet,petID,phonenum)
+VALUES ('$ownID', '$fname', '$email', '$psw','$oPet','$petID','$phonenum')";
 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-include 'connet.php';
+$conn->close();
+include 'connect.php';
 ?>
+
+
