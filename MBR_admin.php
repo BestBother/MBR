d43a06fac46c83607a8b91dd3a697e30502a30b8
SleepyAFR <91980732+SleepyAFR@users.noreply.github.com>
2022-10-26 11:40:04 -0400

    Update MBR_admin.php

--- MBR_admin.php
+++ MBR_admin.php
@@ -17,6 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
+        echo "<input type=\"radio\" id=\"".$row["ownID"]. "\" name=\"oName\" value=\"". $row["ownID"]. "\">
         echo "<br> id: ". $row["ownID"]. " - Name: ".$row["oName"]. "<br>";
     }
 } else {
