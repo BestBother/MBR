f670fc9263ca0519cb4c0484f1b67e8702de31e5
Keshawn Bryant <91396292+BestBother@users.noreply.github.com>
2022-10-31 11:08:22 -0400

    Update MBR_admin.php

--- MBR_admin.php
+++ MBR_admin.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 $servername = "localhost";
 $username = "Bother";
@@ -11,25 +15,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT ownID, oName FROM owner";
-$result = $conn->query($sql);
+//include 'connect.php';
+
+ $sql = "SELECT ownID, oName FROM owner";
+ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "<form action = \"delete_or_update.php\" method = \"post\">";
     // output data of each row
     while($row = $result->fetch_assoc()) {
+		
         echo "<input type=\"radio\" id=\"".$row["ownID"]. "\" name=\"oName\" value=\"". $row["ownID"]. "\">";
         echo "<br> id: ". $row["ownID"]. " - Name: ".$row["oName"]. "<br>";
+        
     }
-    
+
     echo "<br>";
     echo "<input type=\"submit\" value=\"Delete record\" name=\"delete\">";
     echo "<input type=\"submit\" value=\"Update record\" name=\"update\">";
 
     echo "</form>";
+
 } else {
     echo "0 results";
 }
 
 $conn->close();
-include 'connect.php';
 ?>
+
+</body>
+</html>
